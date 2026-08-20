2465d5cb14b82488dc5e87b0eeb81ca4b2f7196b
VoGiaHuy <huyvog6226@gmail.com>
2026-08-20 14:28:39 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-7xl font-bold text-blue-600">
+        HertApp
+      </h1>
+    </div>
+  )
+}
+
+export default App
